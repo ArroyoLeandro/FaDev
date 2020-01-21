3db32e97b570170746d577b760a700f5e0388de2
ArroyoLeandro <martin_arroyo@hotmail.es>
2020-01-21 12:18:00 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
+import { HeaderComponent } from './componentes/header/header.component';
+import { ServicioComponent } from './componentes/servicio/servicio.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ServicioComponent,
+    FooterComponent,
+    ProyectosComponent,
+    NosotrosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule} from '@angular/core';
+
+import { 
+
+    MatButtonModule,
+    MatToolbarModule
+    
+} from '@angular/material';
+
+
+
+ @NgModule({
+    imports: [
+         MatButtonModule,
+         MatToolbarModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatToolbarModule
+    ]
+
+})
+
+export class MaterialModule {}
